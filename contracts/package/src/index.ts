ce79ba7b0766488c26b49c5560dfc71574aa1022
RetricSu <inneverland2013@gmail.com>
2025-09-24 16:06:29 +0800

    feat: validate name and version (#6)
    * feat: validate name and version
    
    * refactor: apply review suggestion

--- contracts/package/src/index.ts
+++ contracts/package/src/index.ts
@@ -1,6 +1,8 @@
 import * as bindings from "@ckb-js-std/bindings";
 import { HighLevel, log } from "@ckb-js-std/core";
 import { PackageDataCodec } from "./type";
+import { decodeBytes20ToUtf8, isCellPresent } from "./util";
+import { isValidSemver, compareSemver } from "./semver";
 
 function main(): number {
   log.setLevel(log.LogLevel.Debug);
@@ -14,7 +16,8 @@ function main(): number {
   const data = HighLevel.loadCellData(0, HighLevel.SOURCE_GROUP_OUTPUT);
   const packageData = PackageDataCodec.decode(data);
   log.debug(`package data: ${JSON.stringify(packageData)}`);
-
+  log.debug(`package name: ${decodeBytes20ToUtf8(packageData.name)}`);
+  log.debug(`package version: ${decodeBytes20ToUtf8(packageData.version)}`);
   // 2. check cellDeps code hash matches with the chunk hash for data availability
   for (const chunk of packageData.chunks) {
     const idx = +chunk.index;
@@ -37,10 +40,40 @@ function main(): number {
   // Instead, we can compute a merkle root hash from the chunk hashes and validate it here.
   // For now we just do nothing with the hash
 
-  // 4. check the package name and version
-  // todo: name should not be changed after created
-  // version should follow semver
-  // For now, we just do nothing
+  // 4. check package name and version
+  const outputVersion = decodeBytes20ToUtf8(packageData.version);
+  if (!isValidSemver(outputVersion)) {
+    log.error(`package version is not valid semver: ${outputVersion}`);
+    return 3;
+  }
+
+  if (isCellPresent(0, bindings.SOURCE_GROUP_INPUT)) {
+    const inputData = HighLevel.loadCellData(0, HighLevel.SOURCE_GROUP_INPUT);
+    const inputPackageData = PackageDataCodec.decode(inputData);
+
+    // 4.1 check the package name: name should not be changed after created
+    if (inputPackageData.name !== packageData.name) {
+      log.error(
+        `package name cannot be changed: ${decodeBytes20ToUtf8(inputPackageData.name)} -> ${decodeBytes20ToUtf8(packageData.name)}`,
+      );
+      return 2;
+    }
+
+    // 4.2 check package version: should follow semver and be greater than the previous version
+    const inputVersion = decodeBytes20ToUtf8(inputPackageData.version);
+    if (!isValidSemver(inputVersion)) {
+      log.error(
+        `previous package version is not valid semver: ${inputVersion}`,
+      );
+      return 4;
+    }
+    if (compareSemver(outputVersion, inputVersion) <= 0) {
+      log.error(
+        `package version must be greater than previous version: ${inputVersion} -> ${outputVersion}`,
+      );
+      return 5;
+    }
+  }
   return 0;
 }
 
